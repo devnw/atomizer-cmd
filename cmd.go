e8a47632e2491e8ff753add4e531bcaff58f7bed
Benji Vesterby <benji@devnw.com>
2021-01-05 18:30:28 -0500

    fixed staticcheck issue

--- cmd.go
+++ cmd.go
@@ -126,10 +126,6 @@ func Initialize() {
 
 	// Create a copy of the atomizer
 	a := atomizer.Atomize(ctx, events)
-	if a == nil {
-		fmt.Println("atomizer instance returned nil")
-		os.Exit(1)
-	}
 
 	// Execute the processing on the atomizer
 	err = a.Exec()

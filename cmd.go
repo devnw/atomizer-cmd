c8db1196fa5fdd52f87327be22f35e69631f7f94
Benji Vesterby <benji@devnw.com>
2021-03-10 06:37:05 -0500

    Adding new linters, actions, and refactored
    Resolves GH-2

--- cmd.go
+++ cmd.go
@@ -32,41 +32,20 @@ const (
 	queueENV string = "QUEUE"
 )
 
+var ConnectionTimeout = time.Second * 60
+var Retries int = 30
+var RetryDelay = time.Second
+
 // Initialize reads flags in from the command line and stands up the atomizer
-func Initialize(appname string) {
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+func Initialize(appname string) error {
 	ctx, cancel := context.WithCancel(context.Background())
 
-	// Default empty appname
-	if appname == "" {
-		appname = "Atomizer Agent"
-	}
+	// Monitor for sigterm
+	sig(ctx, cancel)
 
-	// Setup interrupt monitoring for the agent
-	go func() {
-		defer cancel()
-
-		select {
-		case <-ctx.Done():
-			return
-		case <-sigs:
-			alog.Printf("Closing %s", appname)
-			os.Exit(1)
-		}
-	}()
-
-	err := alog.Global(
-		ctx,
-		appname,
-		alog.DEFAULTTIMEFORMAT,
-		time.UTC,
-		alog.DEFAULTBUFFER,
-		alog.Standards()...,
-	)
-
+	err := initLogger(ctx, appname)
 	if err != nil {
-		fmt.Println("unable to overwrite the global logger")
+		return fmt.Errorf("error setting logger: %s", err.Error())
 	}
 
 	// Parse the command line flags or environment variables
@@ -74,48 +53,31 @@ func Initialize(appname string) {
 
 	u, err := url.Parse(cstring)
 	if err != nil {
-		fmt.Println("error parsing connection string")
-		os.Exit(1)
+		return fmt.Errorf("error parsing connection string | %s", err.Error())
 	}
 
-	if err = waitForHost(ctx, u.Host); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	err = waitForHost(ctx, u, ConnectionTimeout)
+	if err != nil {
+		return err
 	}
 
-	var conductor engine.Conductor
-	var attempts int
-
-	for attempts < 5 && conductor == nil {
-
-		// Create the amqp conductor for the agent
-		conductor, err = amqp.Connect(ctx, cstring, queue)
-		if err != nil || conductor == nil {
-			if attempts >= 5 {
-				fmt.Println(
-					"error while initializing amqp | " +
-						err.Error(),
-				)
-				os.Exit(1)
-			}
-
-			attempts++
-			alog.Printf(
-				"connection attempt %d to amqp failed, retrying",
-				attempts,
-			)
-			time.Sleep(time.Second * 5)
-		}
+	conductor, err := connect(
+		ctx,
+		cstring,
+		queue,
+		Retries,
+		RetryDelay,
+		ConnectionTimeout,
+	)
+	if err != nil {
+		return fmt.Errorf("error creating connection to AMQP | %s", err.Error())
 	}
-
-	alog.Println("connection to amqp established")
 
 	// Register the conductor into the atomizer library after initializing the
 	// connection to the message queue
 	err = engine.Register(conductor)
 	if err != nil {
-		fmt.Println("error registering amqp conductor | " + err.Error())
-		os.Exit(1)
+		return fmt.Errorf("error registering amqp conductor | %s", err.Error())
 	}
 
 	// setup the alog event subscription
@@ -127,15 +89,13 @@ func Initialize(appname string) {
 	// Create a copy of the atomizer
 	a, err := engine.Atomize(ctx, events)
 	if err != nil {
-		fmt.Println("error while initializing atomizer.| " + err.Error())
-		os.Exit(1)
+		return fmt.Errorf("error while initializing atomizer | %s", err.Error())
 	}
 
 	// Execute the processing on the atomizer
 	err = a.Exec()
 	if err != nil {
-		fmt.Println("error while executing atomizer.| " + err.Error())
-		os.Exit(1)
+		return fmt.Errorf("error while executing atomizer | %s", err.Error())
 	}
 
 	alog.Println("Online")
@@ -143,34 +103,121 @@ func Initialize(appname string) {
 	// Block until the processing is interrupted
 	a.Wait()
 
-	time.Sleep(time.Millisecond * 50)
-
 	// Get the alog wait method to work with the internal channels
 	alog.Wait(true)
+
+	return nil
 }
 
-// waitForHost waits for the port of the conductor to become available
-func waitForHost(ctx context.Context, host string) error {
-	ctx, cancel := context.WithTimeout(ctx, time.Second*60)
+func connect(
+	ctx context.Context,
+	cstring,
+	queue string,
+	retries int,
+	retrydelay time.Duration,
+	timeout time.Duration,
+) (conductor engine.Conductor, err error) {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
+
+	var attempt int
+	tick := time.NewTicker(1)
+	defer tick.Stop()
+
+	for attempt < retries {
+		select {
+		case <-ctx.Done():
+			return nil, context.Canceled
+		case <-tick.C:
+			if attempt == 0 {
+				tick.Reset(retrydelay)
+			}
+
+			// Create the amqp conductor for the agent
+			conductor, err = amqp.Connect(ctx, cstring, queue)
+
+			// Connection established
+			if err == nil && conductor != nil {
+				alog.Println("conductor connection established")
+				return conductor, err
+			}
+
+			attempt++
+			alog.Printf(
+				"connection attempt %d to amqp failed, retrying",
+				attempt,
+			)
+		}
+	}
+
+	return nil, fmt.Errorf("error while initializing amqp | %s", err.Error())
+}
+
+func initLogger(ctx context.Context, appname string) error {
+	// Default empty appname
+	if appname == "" {
+		appname = "Atomizer Agent"
+	}
+
+	return alog.Global(
+		ctx,
+		appname,
+		alog.DEFAULTTIMEFORMAT,
+		time.UTC,
+		alog.DEFAULTBUFFER,
+		alog.Standards()...,
+	)
+}
+
+// sig monitors for interrupts and exits
+func sig(ctx context.Context, cancel context.CancelFunc) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	// Setup interrupt monitoring for the agent
+	go func() {
+		defer cancel()
+
+		select {
+		case <-ctx.Done():
+			return
+		case <-sigs:
+			os.Exit(1)
+		}
+	}()
+}
+
+var ErrTimeout = errors.New("connection timeout exceeded")
+
+// waitForHost waits for the port of the conductor to become available
+func waitForHost(ctx context.Context, host *url.URL, timeout time.Duration) error {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	var attempt int
+	tick := time.NewTicker(1)
+	defer tick.Stop()
 
 	for {
 		select {
 		case <-ctx.Done():
-			return errors.New("connection timeout exceeded")
+			return ErrTimeout
+		case <-tick.C:
+			if attempt == 0 {
+				tick.Reset(time.Second)
+			}
 
-		default:
-			_, err := net.Dial("tcp", host)
+			conn, err := net.Dial("tcp", host.Host)
 			if err == nil {
+				defer conn.Close()
+
 				alog.Printf("host [%s] detected", host)
 				return nil
 			}
 
-			alog.Printf("waiting for host [%s]", host)
-			time.Sleep(time.Second * 5)
+			attempt++
+			alog.Printf("waiting for host [%s], attempt %d failed", host.Host, attempt)
 		}
 	}
-
 }
 
 func flags() (conductor, queue string) {
@@ -194,7 +241,6 @@ func flags() (conductor, queue string) {
 // environment pulls the environment variables as defined in the constants
 // section and overwrites the passed flag values
 func environment(cflag, qflag string) (c, q string) {
-
 	c = os.Getenv(connectionStringENV)
 	if c == "" {
 		c = cflag
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module atomizer.io/cmd
 go 1.16
 
 require (
-	atomizer.io/amqp v1.0.4
-	atomizer.io/engine v1.0.2
-	devnw.com/alog v1.0.4
+	atomizer.io/amqp v1.0.5
+	atomizer.io/engine v1.0.3
+	devnw.com/alog v1.0.6
 	github.com/pkg/errors v0.9.1
 )
